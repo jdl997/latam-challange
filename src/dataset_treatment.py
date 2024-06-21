a878971e9006f444298028f64cef68772e023a9f
Juan Lopez <juandalopezhoyos@hotmail.com>
2024-06-21 16:57:18 -0500

    se termina el reto 2 optimizando tiempo

--- src/dataset_treatment.py
+++ src/dataset_treatment.py
@@ -9,7 +9,7 @@ def dataset_q1(file_path: str):
     # Convierte los datos en un DataFrame
     df = pd.DataFrame(data)
 
-    # Filtra el dataset para solo utilizar id, date y 
+    # Filtra el dataset para solo utilizar id, date y user
     df= df.filter(items=['id','date','user'])
 
     # Aplanar la columna 'user' para extraer 'username' y 'id'
@@ -29,5 +29,18 @@ def dataset_q1(file_path: str):
 
     # Convertir la columna 'date' a DateTime
     df['date'] = pd.to_datetime(df['date']).dt.date
+
+    return df
+
+def dataset_q2(file_path: str):
+    # Lee el archivo JSON línea por línea y carga cada línea como un objeto JSON
+    with open(file_path, 'r') as file:
+        data = [json.loads(line) for line in file]
+    
+    # Convierte los datos en un DataFrame
+    df = pd.DataFrame(data)
+
+    # Filtra el dataset para solo utilizar content 
+    df= df.filter(items=['content'])
 
     return df
